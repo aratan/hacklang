147c72c055a1dc1c310df008fed99e78d4f5a698
Victor Arbiol <victor.arbiol@gmail.com>
2025-08-14 21:33:26 +0200

    Add files via upload

---
+++ output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double a = 100;
+    double b = 50;
+    if ((a > b)) {
+        printf("%f\n", a);
+    } else {
+        printf("%f\n", b);
+    };
+    return 0;
+}
